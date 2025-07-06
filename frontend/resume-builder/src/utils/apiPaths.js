5f25b81b351a0c9e53df224fed5f302bc8845b97
nishant7929 <nishantpatel7929@gmail.com>
2025-07-07 01:39:56 +0530

    Add bash url env variable in FE

--- frontend/resume-builder/src/utils/apiPaths.js
+++ frontend/resume-builder/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8005";
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8005";
 
 // utils/apiPaths.js
 export const API_PATHS = {
